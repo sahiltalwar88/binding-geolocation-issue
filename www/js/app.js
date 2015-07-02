cfa1cb0e67f2beb4cb4326db5d24878d5f0efd03
Sahil Talwar <sahiltalwar88@gmail.com>
2015-07-02 01:56:45 -0400

    - Got background geolocation working

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 //angular.module('starter', ['ionic'])
-var app = angular.module('test', ['ionic','test.controllers'])
+var app = angular.module('testApp', ['ionic','testApp.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,13 +1,10 @@
 // Define the angular module
-angular.module('test.controllers', ['ionic', 'ngCordova.plugins.geolocation',
-    'ngCordova.plugins.backgroundGeolocation'])
+angular.module('testApp.controllers', ['ionic', 'ngCordova.plugins.geolocation', 'ngCordova.plugins.backgroundGeolocation'])
 	
-.controller('MapCtrl', ['$scope', '$ionicPopup', '$cordovaGeolocation', '$cordovaBackgroundGeolocation', '$timeout', '$http', function ($scope, $ionicPopup, $cordovaGeolocation, $cordovaBackgroundGeolocation, $timeout, $http) {
+.controller('MapCtrl', ['$scope', '$ionicPopup', '$cordovaGeolocation', '$cordovaBackgroundGeolocation', '$timeout', '$http', 
+	           function ($scope, $ionicPopup, $cordovaGeolocation, $cordovaBackgroundGeolocation, $timeout, $http) {
 	$scope.loaded = false;
-	var options = {
-		// https://github.com/christocracy/cordova-plugin-background-geolocation#config
-    };
-	
+
 	var posOptions = { timeout: 5000, enableHighAccuracy: true, maximumAge: 5000 };
 	$cordovaGeolocation.getCurrentPosition(posOptions)
 		.then(function (location) {
@@ -17,36 +14,37 @@ angular.module('test.controllers', ['ionic', 'ngCordova.plugins.geolocation',
 			//console.log($scope.currentLat, $scope.currentLong);
 		});
 		
-		document.addEventListener("deviceready", onDeviceReady, false);
+	document.addEventListener("deviceready", onDeviceReady, false);
+	
+	function onDeviceReady() {
+		var bgGeo = $cordovaBackgroundGeolocation;
 		
-		function onDeviceReady() {
-			var bgGeo = $cordovaBackgroundGeolocation;
-			
-			// BackgroundGeoLocation is highly configurable.
-			bgGeo.configure({
-				url: 'http://ottoplayapi-prod.elasticbeanstalk.com/user/addVisit/',
-				params: {
-					deviceId: "0502fd09-09e7-4756-97fa-7a0106d3fd45",
-					"location": {
-						"latitude": "38.896339999999995",
-						"longitude": "-77.08521460000001"
-					}
-				},
-				desiredAccuracy: 10,
-				stationaryRadius: 20,
-				distanceFilter: 30,
-				notificationTitle: 'TestTitle', // <-- android only, customize the title of the notification
-				notificationText: 'TestText', // <-- android only, customize the text of the notification
-				activityType: 'OtherNavigation',
-				debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-				stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
-			});
+		// BackgroundGeoLocation is highly configurable.
+		bgGeo.configure({
+			url: 'http://ottoplayapi-prod.elasticbeanstalk.com/user/addVisit/',
+			params: {
+				deviceId: "testApp",
+				"location": {
+					"latitude": "38.896339999999995",
+					"longitude": "-77.08521460000001"
+				}
+			},
+			desiredAccuracy: 10,
+			stationaryRadius: 20,
+			distanceFilter: 30,
+			notificationTitle: 'TestTitle', // <-- android only, customize the title of the notification
+			notificationText: 'TestText', // <-- android only, customize the text of the notification
+			activityType: 'OtherNavigation',
+			debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+			stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
+		});
 
-			bgGeo.start(); // Sahil: Do we need this?? Configure calls start internally.
-		};
+		bgGeo.start(); // Sahil: Do we need this?? Configure calls start internally.
+	};
 }])
 
-.directive('bgeo', ['$cordovaGeolocation', '$cordovaBackgroundGeolocation', '$http', function ($cordovaGeolocation, $cordovaBackgroundGeolocation, $http) {
+.directive('bgeo', ['$cordovaGeolocation', '$cordovaBackgroundGeolocation', '$http', 
+	       function ($cordovaGeolocation, $cordovaBackgroundGeolocation, $http) {
     return {
 		scope: {
            lat: '=',
@@ -72,6 +70,9 @@ angular.module('test.controllers', ['ionic', 'ngCordova.plugins.geolocation',
 
 
 // ALTERNATIVE DECLARATION OF BACKGROUND GEO
+  // var options = {
+		// https://github.com/christocracy/cordova-plugin-background-geolocation#config
+  // };
   // document.addEventListener("deviceready", function () {
 
     // // `configure` calls `start` internally
